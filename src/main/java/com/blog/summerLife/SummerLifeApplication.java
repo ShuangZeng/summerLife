a8b11a430c5b96a5301530aab6796ec24e1545e4
Arun Magar <arun.mgr1@gmail.com>
2020-10-25 00:52:31 -0500

    Initial commit

---
+++ src/main/java/com/blog/summerLife/SummerLifeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.summerLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/summerLife/SummerLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.summerLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
